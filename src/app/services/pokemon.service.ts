badb04d00e5860e97f098af56bd7b64ab87d9127
Breno Lemos <brenolemos327@gmail.com>
2025-03-18 18:29:25 -0300

    Environment Adjust

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,6 +1,6 @@
 import { PokemonData } from './../models/pokemonData';
 import { Injectable } from '@angular/core';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs'
 
